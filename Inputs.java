3f53009813f29f6075a247b4f43241cd2c38ecda
sajwan05 <surajsajwan328@gmail.com>
2023-07-31 16:32:45 +0530

    Revert "Added .gitignore file to exclude .class file"
    This reverts commit 479afb6925bd1ec3b9b454b94f3208328aea20aa.

--- Inputs.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Inputs {
-    public static void main(String[] args) {
-        Scanner input  = new Scanner(System.in);
-        System.out.println("Please enter your roll number:");
-        System.out.println("Your roll number is "+input.nextInt());
-    }
-}
