479afb6925bd1ec3b9b454b94f3208328aea20aa
sajwan05 <surajsajwan328@gmail.com>
2023-07-31 16:29:29 +0530

    Added .gitignore file to exclude .class file

---
+++ Inputs.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Please enter your roll number:");
+        System.out.println("Your roll number is "+input.nextInt());
+    }
+}
---
+++ Primitives.java
@@ -0,0 +1,23 @@
+public class Primitives {
+    public static void main(String[] args) {
+        //    There are basically 4 type of primitive data types
+//    1.Int 2.Double 3.Char 4.boolean
+        int rollNo = 65;
+       char gender = 'm';
+        float marks = 7.44F;
+//        Double is generally used where we need more precision or larger memory to store
+        double salary = 200000.99;
+//        200k dollars default decimal type in java
+//        long is used for large integer than int and use suffix L/l to end the long literal.
+        long largeInt = 343673738L;
+        short shortInt = 34;
+        byte smallInt = 3;
+        boolean isEligible = true;
+
+//        We also can use wrapper classes instead of primitive data type to work with purely OOPS
+//        Wrapper class is used to get additional functionalities than primitive data type
+        Integer rno = 65;
+
+    }
+
+}
