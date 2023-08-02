72c7892bad5faffb9fe5b1ba955d8e97b62a78de
sajwan05 <surajsajwan328@gmail.com>
2023-08-02 13:29:06 +0530

    Made a program to tell if it is Lowercase or Upper just based on input

--- CaseCheck.java
+++ CaseCheck.java
@@ -1,2 +1,14 @@
-package java1;public class CaseCheck {
+package java1;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a Character:");
+        char ch = in.next().trim().charAt(0);
+
+        if (ch>='A' && ch<='Z')     System.out.println("UpperCase"); //if(ch>=65 && cg<=90)
+        else                    System.out.println("LowerCase");
+    }
 }
