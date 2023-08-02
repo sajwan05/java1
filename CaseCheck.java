915991b6b38098bb35c7db547a1faaa0a74a0ea5
sajwan05 <surajsajwan328@gmail.com>
2023-08-02 13:06:20 +0530

    Finding largest of three numbers

---
+++ CaseCheck.java
@@ -0,0 +1,2 @@
+package java1;public class CaseCheck {
+}
--- Largest.java
+++ Largest.java
@@ -1,2 +1,23 @@
-package java1;public class Largest {
+package java1;
+import java.util.Scanner;
+public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        //Find largest of the three numbers
+//Appproach 3:          int max = Math.max(a,Math.max(b,c));
+//                      System.out.println(max);
+
+        int max =a;
+        if (b > max) {
+            max=b;
+        }if(c>max){
+            max=c;
+        }
+        System.out.println(max);
+    }
 }
